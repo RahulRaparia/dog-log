8b4836305ce00189100ba4c16f96388b82971ecb
Rahul Raparia <rahulraparia.cs@gmail.com>
2025-01-24 00:21:01 +0530

    ho gaya bhai

---
+++ data-collection-app/data-collection-service/src/main/java/com/doglog/data_collection_service/DataCollectionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.doglog.data_collection_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCollectionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataCollectionServiceApplication.class, args);
+	}
+
+}
